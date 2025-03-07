926c751907650c2e68427539061efc7bcf70caa8
ZhangHH233 <143386919+ZhangHH233@users.noreply.github.com>
2025-03-07 12:35:40 +0800

    Delete sotas directory

--- sotas/ChoroidalSeg/Wu_2022.py
+++
@@ -1,9 +0,0 @@
-"""@article{wu2022choroidal,
-  title={Choroidal layer segmentation in OCT images by a boundary enhancement network},
-  author={Wu, Wenjun and Gong, Yan and Hao, Huaying and Zhang, Jiong and Su, Pan and Yan, Qifeng and Ma, Yuhui and Zhao, Yitian},
-  journal={Frontiers in Cell and Developmental Biology},
-  volume={10},
-  pages={1060241},
-  year={2022},
-  publisher={Frontiers Media SA}
-}"""
